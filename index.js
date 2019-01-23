252c4dc9b3a82fcc1cff46a0b2411eeb720925d2
asingjr2 <asingjr2@gmail.com>
2019-01-23 14:51:48 -0500

    wired up base oauth and nodemon.

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
+/** Express will handle http requests with route handlers. */ 
 
-// Express will handle http requests with route handlers.
+/**
+ * Creating strategy instance with config arguments.
+ */
 const app = express();
+
+/**
+ * Adding in hidden keys for use.
+ * Callback url is redirect address when sent from google auth endpoint.
+ * Below will be final code for what to do when oauth is succesffully.
+ */
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => console.log(accessToken))); 
+
 
 app.get('/', (req, res) => {
   res.send({
@@ -10,6 +28,15 @@ app.get('/', (req, res) => {
   });
 })
 
+
+/** Get method takes initial string and then authentication direction. */
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+})
+);
+
+/** Call back handled with include gmail secret session code. */
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT =  process.env.PORT || 5000;
 app.listen(PORT);
